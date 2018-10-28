14f6228f7448b3c443ace2e1fe6839778e430e36
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-10-28 18:25:59 +1100

    Add clients service

---
+++ QuickForecaster.Web/Controllers/ClientsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuickForecaster.Application.Clients.Queries;
+
+namespace QuickForecaster.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ClientsController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClientDto>>> GetAll()
+        {
+            return Ok(await _mediator.Send(new GetAllClientsQuery()));
+        }
+    }
+}
