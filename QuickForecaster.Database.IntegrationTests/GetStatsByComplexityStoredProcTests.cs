3dda659746f8659df70f891f52d61d975d3aae87
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-10-29 14:58:02 +1100

    Include a basic stored proc test

---
+++ QuickForecaster.Database.IntegrationTests/GetStatsByComplexityStoredProcTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using QuickForecaster.Domain.Entities;
+using QuickForecaster.Persistence;
+using StoredProcedureEFCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QuickForecaster.Database.IntegrationTests
+{
+    public class GetStatsByComplexityStoredProcTests
+    {
+        private const string connectionString = @"Server=.\sqlexpress01;Database=QuickForecaster;Trusted_Connection=True;Application Name = QuickForecaster;";
+
+        [Fact]
+        public void SpGetStatsByComplexity_WithExistingBacklogItems_ReturnsAggregatedData()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<QuickForecasterDbContext>();
+            optionsBuilder.EnableSensitiveDataLogging(true);
+            optionsBuilder.UseSqlServer(connectionString);
+
+            using (var context = new QuickForecasterDbContext(optionsBuilder.Options))
+            {
+                context.Database.EnsureCreated();
+
+                List<Stat> stats = null;
+                context.LoadStoredProc("[dbo].[spGetStatsByComplexity]")
+                   .AddParam("EstimateId", 1)
+                   .Exec(r => stats = r.ToList<Stat>());
+
+                stats.Should().NotBeNull();
+                stats.Count.Should().Be(3);
+            }
+        }
+    }
+}
---
+++ QuickForecaster.Database.IntegrationTests/Stat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickForecaster.Database.IntegrationTests
+{
+    public class Stat
+    {
+        public string Confidence { get; set; }
+        public int NumberOfItems { get; set; }
+    }
+}
