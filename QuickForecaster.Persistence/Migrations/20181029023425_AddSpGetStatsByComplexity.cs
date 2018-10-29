49b93013dcc8ad4894f2c8c9e1b4f2ce6ee08d02
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-10-29 13:40:36 +1100

    Add stored procedure to get stats by complexity

---
+++ QuickForecaster.Persistence/Migrations/20181029023425_AddSpGetStatsByComplexity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickForecaster.Persistence.Migrations
+{
+    public partial class AddSpGetStatsByComplexity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[spGetStatsByComplexity]
+	                        @EstimateId int
+                        AS
+                        BEGIN
+	                        SELECT BI.Confidence Confidence, 
+		                        COUNT(BI.Id) NumberOfItems
+	                        FROM [dbo].[BacklogItems] as BI
+	                        WHERE BI.EstimateId = @EstimateId
+	                        GROUP BY BI.Confidence
+                        END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {            
+            var sp = @"DROP PROCEDURE[dbo].[spGetStatsByComplexity]";
+            migrationBuilder.Sql(sp);
+        }
+    }
+}
