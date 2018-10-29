4548d71def7c42ac69b7d8ef737275ba72f02533
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-10-29 11:18:02 +1100

    Add basic integration test for clients api

---
+++ QuickForecaster.Web.IntegrationTests/ClientsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QuickForecaster.Web.IntegrationTests
+{
+    public class ClientsControllerTests : IClassFixture<WebApplicationFactory<QuickForecaster.Web.Startup>>
+    {
+        private readonly WebApplicationFactory<QuickForecaster.Web.Startup> _factory;
+
+        public ClientsControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAllClients_WithExistingClients_ReturnsAll()
+        {
+            var client = _factory.CreateClient(
+                new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false,
+                    BaseAddress = new Uri("http://localhost")
+                });
+
+            var response = await client.GetAsync("http://localhost/api/clients");
+            var content = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.NotNull(content);
+        }
+    }
+}
